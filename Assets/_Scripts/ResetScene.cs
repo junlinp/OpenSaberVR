f3b7f02c709dfb1abb31841c8a5c89410605a72c
junlinp <junlinp@deepmirror.com>
2023-12-18 19:00:53 +0800

    skybox

---
+++ Assets/_Scripts/ResetScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+public class ResetScene : MonoBehaviour
+{
+    public void ReloadCurrentScene() {
+        //StartCoroutine(LoadSongScene);
+    }
+
+    private IEnumerator LoadSongScene() {
+        //SceneManager.LoadScene("Main_Menu");
+        //SceneManager.LoadScene("PersistentScene");
+        return null;
+    }
+}
